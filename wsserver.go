e9a0d6ed2b4169efdb32c7eb4c917e7bcc47ac5d
tuuz <silverhawk@foxmail.com>
2024-10-09 15:20:49 +0800

    1.区分消息类型 2.增加自动pingpong回复类型

--- wsserver.go
+++ wsserver.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
+	"time"
 )
 
 type WsData struct {
@@ -16,12 +17,21 @@ var WsServer_ReadChannel = make(chan WsData, 1)
 var WsServer_WriteChannel = make(chan WsData, 1)
 
 type WsServer struct {
-	url  string
-	err  error
-	Conn *websocket.Conn
+	WsConfig *WsConfig
+	url      string
+	err      error
+	Conn     *websocket.Conn
+}
+
+type WsConfig struct {
+	PingReplyDelayInMs uint
+	PongReplyDelayInMs uint
 }
 
 func (ws *WsServer) NewServer(w http.ResponseWriter, r *http.Request, responseHeader http.Header) {
+	if ws.WsConfig.PingReplyDelayInMs == 0 {
+		ws.WsConfig.PingReplyDelayInMs = 10
+	}
 	upd := websocket.Upgrader{}
 	upd.EnableCompression = false
 	upd.CheckOrigin = func(r *http.Request) bool {
@@ -51,11 +61,17 @@ func (ws *WsServer) NewServer(w http.ResponseWriter, r *http.Request, responseHe
 			break
 
 		case websocket.PingMessage:
-			WsServer_ReadChannel <- WsData{Conn: ws.Conn, Message: message, Status: true}
+			go func() {
+				time.Sleep(time.Duration(ws.WsConfig.PongReplyDelayInMs) * time.Millisecond)
+				ws.Conn.WriteMessage(websocket.PongMessage, []byte("pong"))
+			}()
 			break
 
 		case websocket.PongMessage:
-			WsServer_ReadChannel <- WsData{Conn: ws.Conn, Message: message, Status: true}
+			go func() {
+				time.Sleep(time.Duration(ws.WsConfig.PingReplyDelayInMs) * time.Millisecond)
+				ws.Conn.WriteMessage(websocket.PingMessage, []byte("ping"))
+			}()
 			break
 
 		case websocket.CloseMessage:
