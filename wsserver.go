a1d148bc14732bfc4d717ca5c4ffeb3170517f40
tuuz <silverhawk@foxmail.com>
2024-10-09 16:21:06 +0800

    1.区分消息类型 2.增加自动pingpong回复类型 3.新增关闭消息，让WS通道可以正常回收，避免炸内存

--- wsserver.go
+++ wsserver.go
@@ -73,15 +73,23 @@ func (ws *WsServer) NewServer(w http.ResponseWriter, r *http.Request, responseHe
 			break
 
 		case websocket.CloseMessage, -1:
-			select {
-			case <-time.After(1 * time.Second):
-				break
-			case WsServer_WriteChannel <- WsData{Conn: ws.Conn, Message: message, Type: Type}:
-				break
-			case WsServer_ReadChannel <- WsData{Conn: ws.Conn, Message: message, Type: Type}:
-				break
-			}
+			go func() {
+				select {
+				case <-time.After(1 * time.Second):
+					break
+				case WsServer_WriteChannel <- WsData{Conn: ws.Conn, Message: message, Type: Type}:
+					break
+				}
+			}()
 
+			go func() {
+				select {
+				case <-time.After(1 * time.Second):
+					break
+				case WsServer_ReadChannel <- WsData{Conn: ws.Conn, Message: message, Type: Type}:
+					break
+				}
+			}()
 			return
 
 		default:
